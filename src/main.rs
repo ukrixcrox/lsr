ba2afb535e23ae19c97553f0bc271a168075a0fd
Falk-Witte <krixcrox@duck.com>
2023-03-23 18:20:18 +0100

    some refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use clap::Parser;
 
 mod format;
+mod utils;
 
 /// simple ls implementation in rust for the crust shell
 #[derive(Parser)]
@@ -13,11 +14,13 @@ struct Opts{
     dir:String,
 
     /// shows everything thats in a dir
-    #[arg(short='a', long)]
+    #[arg(short='a')]
+    #[arg(long="show-all")]
     show_all:bool,
 
     /// shows only dirs
-    #[arg(short='d', long)]
+    #[arg(short='d')]
+    #[arg(long="dir-only")]
     dir_only:bool,
 
     //TODO: need to add the long format before adding color for executable files
@@ -39,19 +42,21 @@ fn main() {
                                         .unwrap();
 
                 // works for now
+                // normal output
                 if !args.show_all && !args.dir_only{
                     if !entry_string.starts_with('.'){
                         format::output(&entry).unwrap();
                     }
-
+                // all flag output
                 }else if args.show_all && !args.dir_only{
                     format::output(&entry).unwrap();
 
+                // all flag and dir_only flag output
                 }else if args.show_all && args.dir_only{
                     if entry.is_dir(){
                         format::output(&entry).unwrap();
                     }
-
+                // dir_only flag output
                 }else if !args.show_all && args.dir_only && entry.is_dir() && !entry_string.starts_with('.'){
                         format::output(&entry).unwrap();
                     }
---
+++ src/utils.rs
@@ -0,0 +1,8 @@
+use std::fs::metadata;
+use std::path::Path;
+
+/// returns the last time the file was modified as String
+pub fn get_file_modified_time(file: &Path) -> String {
+    //TODO: implement function
+    return String::from("");
+}
