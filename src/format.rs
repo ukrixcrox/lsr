656c2e594e72e5bd993846b7e17b21e3a58a5926
Falk-Witte <krixcrox@duck.com>
2023-03-27 17:54:39 +0200

    add mtime

--- src/format.rs
+++ src/format.rs
@@ -3,8 +3,10 @@ use colored::Colorize;
 use termion::raw::IntoRawMode;
 use termion::cursor;
 use std::io::{self, stdout, Write};
+use crate::utils::get_mtime;
 
 pub fn output(entry: &Path) -> io::Result<()> {
+    let mtime = get_mtime(entry);
 
     // setting up terminal in raw mode 
     let stdout = stdout().into_raw_mode()?;
@@ -17,13 +19,13 @@ pub fn output(entry: &Path) -> io::Result<()> {
                             .unwrap();
     
     if entry_string.starts_with('.') && !entry.is_dir(){
-        write!(stdout, "\n{}{}", cursor::Left(100), entry_string.red())?;
+        write!(stdout, "\n{}{} {}",cursor::Left(100),mtime, entry_string.red())?;
     }else if entry.is_dir(){
-        write!(stdout, "\n{}{}", cursor::Left(100), entry_string.bold().blue())?;
+        write!(stdout, "\n{}{} {}", cursor::Left(100),mtime, entry_string.bold().blue())?;
     }else if entry.is_symlink(){
-        write!(stdout, "\n{}{}", cursor::Left(100), entry_string.yellow())?;
+        write!(stdout, "\n{}{} {}", cursor::Left(100), mtime, entry_string.yellow())?;
     }else{
-        write!(stdout, "\n{}{}", cursor::Left(100), entry_string)?;
+        write!(stdout, "\n{}{} {}", cursor::Left(100), mtime, entry_string)?;
     }
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,17 @@
 use std::fs::metadata;
 use std::path::Path;
+use filetime::FileTime;
+use chrono::NaiveDateTime;
 
-/// returns the last time the file was modified as String
-pub fn get_file_modified_time(file: &Path) -> String {
-    //TODO: implement function
-    return String::from("");
+
+/// returns the last time the file was modified as String type
+pub fn get_mtime(file: &Path) -> String {
+    let metadata = metadata(file).unwrap();
+    let mtime = FileTime::from_last_modification_time(&metadata).seconds();
+
+    // formatted mtime
+    NaiveDateTime::from_timestamp_opt(mtime, 0).unwrap()
+                                            .format("%Y-%m-%d %H:%M:%S")
+                                            .to_string()
+
 }
