d5530042ea83ae841098f32dec55c9644bc44054
Falk-Witte <krixcrox@duck.com>
2023-04-28 16:50:33 +0200

    add Owner

--- src/format.rs
+++ src/format.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 use colored::{Colorize, ColoredString};
 use crate::utils::{get_mtime, get_size, get_longest_len, get_file_permissions};
 use is_executable::IsExecutable;
+use file_owner::PathExt;
 
 pub fn output(entry: Vec<PathBuf>){
 
@@ -14,10 +15,10 @@ pub fn output(entry: Vec<PathBuf>){
     let file_name_len = get_longest_len(file_name_len_vec);
     println!("{}", file_name_len);
     
-    let row_end_string = "-".repeat(41+file_size_len+file_name_len);
+    let row_end_string = "-".repeat(49+file_size_len+file_name_len);
 
     println!("{}", row_end_string);
-    println!("{:<11} | {:<width$} | {:<19} | {:width_file_name$} |", "Permissions", "Size", "Last modified", "Name", width=file_size_len, width_file_name=file_name_len);
+    println!("{:<11} | {:5} | {:<width$} | {:<19} | {:width_file_name$} |", "Permissions", "Owner", "Size", "Last modified", "Name", width=file_size_len, width_file_name=file_name_len);
     println!("{}", row_end_string);
 
     for entry in entry{
@@ -31,6 +32,9 @@ pub fn output(entry: Vec<PathBuf>){
                                 .into_string()
                                 .unwrap();
 
+        let file_owner:String = entry_string.owner().unwrap().name().unwrap().unwrap();
+        let file_group:String = entry_string.group().unwrap().name().unwrap().unwrap();
+
         // format file size/name ouput to always right align 
         let file_size_output = format!("{:>width$}", file_size, width = file_size_len);
         let file_name_output = format!("{:<width$}", entry_string, width=file_name_len);
@@ -38,20 +42,20 @@ pub fn output(entry: Vec<PathBuf>){
         //idk
         //at least it works
         if entry_string.starts_with('.') && !entry.is_dir(){
-            print_output(&file_name_output.cyan(), &file_size_output, &file_permissions, &mtime)
+            print_output(&file_name_output.cyan(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
         }else if entry.is_dir(){
-            print_output(&file_name_output.bold().blue(), &file_size_output, &file_permissions, &mtime)
+            print_output(&file_name_output.bold().blue(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
         }else if entry.is_symlink(){
-            print_output(&file_name_output.yellow(), &file_size_output, &file_permissions, &mtime)
+            print_output(&file_name_output.yellow(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
         }else if entry.is_executable(){
-            print_output(&file_name_output.bold().green(), &file_size_output, &file_permissions, &mtime)
+            print_output(&file_name_output.bold().green(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
         }else{
-            print_output(&file_name_output.white(), &file_size_output, &file_permissions, &mtime)
+            print_output(&file_name_output.white(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
         }
     }
     println!("{}", row_end_string);
 }
 
-fn print_output(entry_string:&ColoredString, file_size_output:&String, file_permissions:&String, mtime:&String){
-        println!("{:<11} | {} | {:>19} | {} |", file_permissions.blue(), file_size_output.purple(), mtime.yellow(), entry_string);
+fn print_output(entry_string:&ColoredString, file_size_output:&String, file_permissions:&String, mtime:&String, file_owner:&String, file_group:&String){
+        println!("{:<11} | {:2} {:2} | {} | {:>19} | {} |", file_permissions.blue(), file_group.green(), file_owner.green(), file_size_output.purple(), mtime.yellow(), entry_string);
 }
