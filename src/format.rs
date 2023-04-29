e68ae8e1f13a8122dd061948ca42322ccb7c8a71
Falk-Witte <krixcrox@duck.com>
2023-04-29 20:22:47 +0200

    create more bugs

--- src/format.rs
+++ src/format.rs
@@ -6,19 +6,22 @@ use file_owner::PathExt;
 
 pub fn output(entry: Vec<PathBuf>){
 
-    // collect all the file_sizes/name in a vec
+    // collect all the file_sizes/name/group/owner in a vec
     let file_size_len_vec: Vec<usize> = entry.iter().map(|entry| get_size(entry).len()).collect();
     let file_name_len_vec: Vec<usize> = entry.iter().map(|entry| entry.file_name().unwrap().to_owned().into_string().unwrap().len()).collect();
+    let file_group_name_len_vec: Vec<usize> = entry.iter().map(|entry| entry.file_name().unwrap().to_owned().into_string().unwrap().group().unwrap().name().unwrap().unwrap().len()).collect();
+    let file_owner_name_len_vec: Vec<usize> = entry.iter().map(|entry| entry.file_name().unwrap().to_owned().into_string().unwrap().owner().unwrap().name().unwrap().unwrap().len()).collect();
 
-    // get the longes file_size/name len
+    // get the longes file_size/name/group/owner len
     let file_size_len = get_longest_len(file_size_len_vec);
     let file_name_len = get_longest_len(file_name_len_vec);
-    println!("{}", file_name_len);
+    let file_group_name_len = get_longest_len(file_group_name_len_vec);
+    let file_owner_name_len = get_longest_len(file_owner_name_len_vec);
     
-    let row_end_string = "-".repeat(49+file_size_len+file_name_len);
+    let row_end_string = "-".repeat(45+file_size_len+file_name_len+file_group_name_len+file_owner_name_len);
 
     println!("{}", row_end_string);
-    println!("{:<11} | {:5} | {:<width$} | {:<19} | {:width_file_name$} |", "Permissions", "Owner", "Size", "Last modified", "Name", width=file_size_len, width_file_name=file_name_len);
+    println!("{:<11} | {:width_owner_name$} | {:<width$} | {:<19} | {:width_file_name$} |", "Permissions", "Owner", "Size", "Last modified", "Name", width=file_size_len, width_file_name=file_name_len, width_owner_name=file_owner_name_len+file_group_name_len+1);
     println!("{}", row_end_string);
 
     for entry in entry{
@@ -32,25 +35,27 @@ pub fn output(entry: Vec<PathBuf>){
                                 .into_string()
                                 .unwrap();
 
-        let file_owner:String = entry_string.owner().unwrap().name().unwrap().unwrap();
         let file_group:String = entry_string.group().unwrap().name().unwrap().unwrap();
+        let file_owner:String = entry_string.owner().unwrap().name().unwrap().unwrap();
 
-        // format file size/name ouput to always right align 
+        // format file size/name ouput to have enough width 
         let file_size_output = format!("{:>width$}", file_size, width = file_size_len);
         let file_name_output = format!("{:<width$}", entry_string, width=file_name_len);
+        let file_group_output = format!("{:<width$}", file_group, width=file_group_name_len);
+        let file_owner_output = format!("{:width$}", file_owner, width=file_owner_name_len);
         
         //idk
         //at least it works
         if entry_string.starts_with('.') && !entry.is_dir(){
-            print_output(&file_name_output.cyan(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
+            print_output(&file_name_output.cyan(), &file_size_output, &file_permissions, &mtime, &file_owner_output, &file_group_output)
         }else if entry.is_dir(){
-            print_output(&file_name_output.bold().blue(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
+            print_output(&file_name_output.bold().blue(), &file_size_output, &file_permissions, &mtime, &file_owner_output, &file_group_output)
         }else if entry.is_symlink(){
-            print_output(&file_name_output.yellow(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
+            print_output(&file_name_output.yellow(), &file_size_output, &file_permissions, &mtime, &file_owner_output, &file_group_output)
         }else if entry.is_executable(){
-            print_output(&file_name_output.bold().green(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
+            print_output(&file_name_output.bold().green(), &file_size_output, &file_permissions, &mtime, &file_owner_output, &file_group_output)
         }else{
-            print_output(&file_name_output.white(), &file_size_output, &file_permissions, &mtime, &file_owner, &file_group)
+            print_output(&file_name_output.white(), &file_size_output, &file_permissions, &mtime, &file_owner_output, &file_group_output)
         }
     }
     println!("{}", row_end_string);
